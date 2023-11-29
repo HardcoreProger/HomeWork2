135ba3967f4816466d54ceef1127a9e027bd7abc
user <ilasmyga26@gmail.com>
2023-11-29 22:41:05 +0300

    hw done

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Exercise2 {
     public static void main (String [] args){
 
+            Scanner input = new Scanner(System.in);
+            System.out.println("\nEnter seconds");
+            int inpt = input.nextInt(), sec = inpt % 60, min = (inpt/60) % 60, hrs = (inpt/60) /60;
+            System.out.println("\ntime " + hrs + ":" + min + ":" + sec);
+        }
     }
-}
+
